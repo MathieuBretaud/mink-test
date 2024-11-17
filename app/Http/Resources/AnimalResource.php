3d80d38fd28882fbf207463ba8145ace030f6802
MathieuBretaud <mathieu.bretaud@gmail.com>
2024-11-17 17:22:26 +0100

    add resource animal breed type

---
+++ app/Http/Resources/AnimalResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AnimalResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $TVA = 0.20; // Taux de TVA de 20%
+        $priceWithVat = $this->price * (1 + $TVA); // Calcul du prix avec TVA
+
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'age' => $this->age,
+            'description' => $this->description,
+            'price' => round($priceWithVat, 2),
+            'status' => __('status.' . $this->status),
+            'type' => new TypeResource($this->type),
+            'breed' => new BreedResource($this->breed)
+        ];
+    }
+}
