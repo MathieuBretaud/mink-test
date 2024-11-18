1074d756bcb82da41b87dddecbd149cc3e88d86b
MathieuBretaud <mathieu.bretaud@gmail.com>
2024-11-18 09:38:01 +0100

    composable filter

---
+++ resources/js/composables/useAnimals.js
@@ -0,0 +1,38 @@
+import {ref} from "vue";
+
+export function useAnimals () {
+    const animals = ref([]);
+    const orderBy = ref(null);
+    const direction = ref('desc');
+
+    const getAnimals = async () => {
+        try {
+            let { data } = await axios.get('/api/animals', {
+                params: {
+                    orderBy: orderBy.value,
+                    direction: direction.value,
+                }
+            });
+            animals.value = data.data;
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    const toggleDirection = () => direction.value = direction.value === 'asc' ? 'desc' : 'asc';
+
+    const toggleOrder = async (criteria) => {
+        orderBy.value = criteria;
+
+        toggleDirection();
+
+        await getAnimals();
+    }
+
+    return {
+        animals,
+        getAnimals,
+        toggleOrder,
+        orderBy,
+    }
+}
