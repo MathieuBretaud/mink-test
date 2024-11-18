9293b847a5f4c8cf63163cb1bb18e2852cc5d540
MathieuBretaud <mathieu.bretaud@gmail.com>
2024-11-18 11:59:27 +0100

    add command create user done

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new user interactively';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $name = $this->ask('Enter your name');
+        $email = $this->ask('Enter your email');
+        $password = $this->secret('Enter your password');
+
+        $user = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+        $this->info("User created successfully with ID: {$user->id}");
+    }
+}
