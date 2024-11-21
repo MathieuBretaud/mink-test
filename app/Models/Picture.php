4559586926ae3de024f5ad9dcc6a2dd5a3916872
MathieuBretaud <mathieu.bretaud@gmail.com>
2024-11-21 12:57:26 +0100

    event delete picture and getImageUrl for resize glide

--- app/Models/Picture.php
+++ app/Models/Picture.php
@@ -3,11 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use League\Glide\Urls\UrlBuilderFactory;
 
 /**
  * @mixin IdeHelperPicture
  */
 class Picture extends Model
 {
-    //
+    protected $fillable = [
+        'filename',
+    ];
+
+    protected static function booted(): void
+    {
+        static::deleting(function (Picture $picture) {
+            Storage::disk('public')->delete($picture->filename);
+        });
+    }
+
+    public function getImageUrl(?int $width = null, ?int $height = null): string
+    {
+        if ($width === null) {
+            return Storage::disk('public')->url($this->filename);
+        }
+        $urlBuilder = UrlBuilderFactory::create('/images/', config('glide.key'));
+        return $urlBuilder->getUrl($this->filename, ['w' => $width, 'h' => $height, 'fit' => 'crop']);
+    }
 }
